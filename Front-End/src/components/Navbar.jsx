2a4a6a8c059aeb02f617d377a9a9d45b59e2f1b4
Aryan <bhardwaj7a@gmail.com>
2025-07-18 10:51:07 +0530

    Added the search bar component for the collections page

--- Front-End/src/components/Navbar.jsx
+++ Front-End/src/components/Navbar.jsx
@@ -1,8 +1,10 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import {assets} from "../assets/assets"
 import { Link, NavLink } from "react-router-dom";
+import { ShopContext } from "../context/ShopContext";
 const Navbar = () => {
     const [visible,setVisible] = useState(false);
+    const {setShowSearch} = useContext(ShopContext);
     return (
         <div className="flex items-center justify-between py-5 font-medium">
        <Link to="/"> <img src={assets.logo} className="w-36" /> </Link>
@@ -12,7 +14,7 @@ const Navbar = () => {
             <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
            </NavLink>
            <NavLink to="/collection" className="flex flex-col items-center gap-1">
-            <p>COLLECTION</p>
+            <p>COLLECTIONS</p>
             <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
            </NavLink>
            <NavLink to="/about" className="flex flex-col items-center gap-1">
@@ -25,7 +27,7 @@ const Navbar = () => {
            </NavLink>
         </ul>
         <div className="flex items-center gap-6">
-            <img src={assets.search_icon} className="w-5 cursor-pointer" alt="" />
+            <img onClick={() => setShowSearch(true)} src={assets.search_icon} className="w-5 cursor-pointer" alt="" />
             <div className="group relative">
                 <img src={assets.profile_icon} className="w-5 cursor-pointer" alt="" />
                 <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4">
