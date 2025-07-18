cc8105db04b06f652117e6da3708fca79b518875
Aryan <bhardwaj7a@gmail.com>
2025-07-18 10:55:45 +0530

    Write the logic for making the search bar functional

--- Front-End/src/pages/Collection.jsx
+++ Front-End/src/pages/Collection.jsx
@@ -40,6 +40,11 @@ const Collection = ()=> {
     const applyFilter = () => {
         let productsCopy = products.slice();
 
+        if(showSearch && search)
+        {
+            productsCopy = productsCopy.filter((item) => item.name.toLowerCase().includes(search.toLowerCase()))
+        }
+
         if(category.length > 0)
         {
             productsCopy = productsCopy.filter((item) => category.includes(item.category) )
@@ -74,7 +79,7 @@ const Collection = ()=> {
 
     useEffect(() => {
        applyFilter();
-    },[category,subCategory])
+    },[category,subCategory,search,showSearch])
 
     useEffect(() => {
     sortProduct();
