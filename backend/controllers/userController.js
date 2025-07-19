c321004a96e43e257179498185c45703dddd38b0
Aryan <bhardwaj7a@gmail.com>
2025-07-19 20:39:04 +0530

    Added the route for user Sign Up

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,3 +1,14 @@
+import userModel from "../models/userModel.js";
+import validator from "validator";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken"
+
+const createToken = (id) => {
+    return jwt.sign({
+        id : id
+    },process.env.JWT_SECRET)
+}
+
 // Route for user login 
 const loginUser = async (req,res) => {
 
@@ -5,6 +16,59 @@ const loginUser = async (req,res) => {
 
 // Route for user register
 const registerUser = async (req,res) => {
+try{
+const {name,email,password} = req.body;
+//checking if user already exists
+const exists = await userModel.findOne({
+    email : email
+})
+if(exists)
+{
+  return res.json({
+    success:false,
+    message:"user already exists"
+  })
+}
+
+// Validating email format and strong password
+if(!validator.isEmail(email))
+{
+    return res.json({
+        success:false,
+        message:"Please enter a valid email"
+    })
+}
+if(password.length < 8)
+{
+    return res.json({
+        success:false,
+        message:"Please enter a strong password"
+    })
+}
+
+// Hashing user password
+const salt = await bcrypt.genSalt(10);
+const hashedPassword = await bcrypt.hash(password,salt)
+
+const newUser = new userModel({
+    name,email,
+    password : hashedPassword
+})
+
+const user = await newUser.save()
+
+const token = createToken(user._id)
+res.json({
+    success:true,
+    token : token
+})
+}catch(error){
+  console.log(error);
+  res.json({
+    success : false,
+    message : error.message
+  })
+}
 
 }
 
