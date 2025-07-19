b91f3327a9698f8c80d1d693c3259f117e7bf86e
Aryan <bhardwaj7a@gmail.com>
2025-07-19 20:05:37 +0530

    Created routing structure using Express router

---
+++ backend/controllers/userController.js
@@ -0,0 +1,16 @@
+// Route for user login 
+const loginUser = async (req,res) => {
+
+}
+
+// Route for user register
+const registerUser = async (req,res) => {
+
+}
+
+// Route for admin Login
+const adminLogin = async (req,res) => {
+     
+}
+
+export {loginUser , registerUser, adminLogin}
