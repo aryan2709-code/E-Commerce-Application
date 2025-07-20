33106cde27865fd0b77fd9ef6612963de83db95c
Aryan <bhardwaj7a@gmail.com>
2025-07-20 12:48:54 +0530

    Added a route for removing a product from the database

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -46,13 +46,30 @@ res.json({success:true, message : "Product added"})
 
 // Function for listing product
 const listProducts = async (req,res) => {
-
+try{
+ const products = await productModel.find({});
+ res.json({success:true,products})
+}catch(error){
+console.log(error)
+res.json({success:false,message:error.message})
+}
 }
 
 // Function for removing product
 const removeProduct = async (req,res) => {
-
-
+   try{
+         await productModel.findByIdAndDelete(req.body.id);
+         res.json({
+            success : true,
+            message : "product removed"
+         })
+   }catch(error){
+     console.log(error);
+     res.json({
+        success : false,
+        message : error.message
+     })
+   }
 }
 
 const singleProduct = async (req,res) => {
