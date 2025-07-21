a8155050c9ea22640ebb9e19425b5d6587b6adda
Aryan <bhardwaj7a@gmail.com>
2025-07-21 15:10:20 +0530

    Added cartRouter to the route structure

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -1,1 +1,16 @@
 // Add Products to the user cart
+const addToCart = async (req,res) => {
+
+}
+
+// Update user cart
+const updateCart = async (req,res) => {
+
+}
+
+// Get user cart data
+const getUserCart = async (req,res) => {
+
+}
+
+export {addToCart , updateCart , getUserCart}
